4bc2c117a405b06db99880e98ae817adf555a92e
JoelT-92 <JoelT-92@users.noreply.github.com>
2016-09-23 20:43:15 +0800

    Update asg2-task2.cpp

--- asg2-task2.cpp
+++ asg2-task2.cpp
@@ -1,1 +1,16 @@
+#include "llvm/DebugInfo.h"
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/SourceMgr.h"
 
+using namespace llvm;
+
+int main(int argc, char **argv) {
+    return 0;
+}
