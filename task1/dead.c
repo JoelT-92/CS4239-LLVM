44a514da16ee941e412b0a3dc605fcf81abf472f
JoelT-92 <JoelT-92@users.noreply.github.com>
2016-10-09 01:30:17 +0800

    Fix implicit declaration nag

--- task1/dead.c
+++ task1/dead.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//#include "dead2.h"
+#include "dead2.h"
 
-/*
-int dead2_complex_function();
-void dead2_maybe_dead_function();
-void dead2_function_always_called();
-void dead2_dead_function();
-*/
 int complex_function();
 void maybe_dead_function();
 void function_always_called();
@@ -17,44 +11,44 @@ void dead_function();
 void (*f)(void);
 
 int main(void) {
-	if (complex_function()) {
-		maybe_dead_function();
-	}
-	function_always_called();
-	if (0) {
-		dead_function();
-	}
+    if (complex_function()) {
+        maybe_dead_function();
+    }
+    function_always_called();
+    if (0) {
+        dead_function();
+    }
 
-	if (dead2_complex_function()) {
-		dead2_maybe_dead_function();
-	}
-	dead2_function_always_called();
-	
-	if (0) {
-		dead2_dead_function();
-	}
-	
-	f = &dead_function;
-	f();
+    if (dead2_complex_function()) {
+        dead2_maybe_dead_function();
+    }
+    dead2_function_always_called();
+    
+    if (0) {
+        dead2_dead_function();
+    }
+    
+    f = &dead_function;
+    f();
 }
 
 int complex_function() {
-	printf("complex_function\n");
-	time_t t;
-	srand((unsigned) time(&t));
-	return (rand() % 2);
+    printf("complex_function\n");
+    time_t t;
+    srand((unsigned) time(&t));
+    return (rand() % 2);
 
 }
 
 void maybe_dead_function() {
-	printf("maybe_dead_function\n");
+    printf("maybe_dead_function\n");
 }
 
 void function_always_called() {
-	printf("function_always_called\n");
+    printf("function_always_called\n");
 }
 
 
 void dead_function() {
-	printf("dead_function\n");
+    printf("dead_function\n");
 }
