f2c5e99b619de1986c0a5ee4132bdeec13da851f
chewyungchung <a0133662@u.nus.edu>
2016-10-09 01:09:03 +0800

    Update task2 check for pointer escape via global pointers

--- task2/asg2-task2.cpp
+++ task2/asg2-task2.cpp
@@ -16,6 +16,8 @@
 using namespace llvm;
 using namespace std;
 
+StringMap<StringRef> globalPtrVarMap;
+
 int main(int argc, char **argv) {
 	
 	// Step (1) Parse all IR files. 
@@ -29,31 +31,65 @@ int main(int argc, char **argv) {
 			fprintf(stderr, "failed to read IR file %s\n", argv[1]);
 			return 1;
 		}
+		
+		// Keep track of all global pointers in globalPtrVarMap
+		Module::GlobalListType &globals = M->getGlobalList();
+		for (Module::GlobalListType::iterator it = globals.begin(), it_end = globals.end(); it != it_end; ++it) {
+			GlobalVariable &globalVar = *it;			
+			Value *castGlobalValue = dyn_cast<Value>(&globalVar);
+			if (castGlobalValue != nullptr) {
+				PointerType *globalVarPtr = dyn_cast<PointerType>(castGlobalValue->getType()->getPointerElementType());
+				if (globalVarPtr != nullptr) {
+					globalPtrVarMap[globalVar.getName()] = "";
+				} 
+			} 
+		}
 	
 		// Step (2) Traverse all instructions
 		for (auto &F: *M) { // For each function F
-			StringMap<StringRef> allocaMap;
-			StringRef latestLoadValue;
-			//cout << "Function name:" << (&F)->getName().str() << endl;
-			for (auto &BB: F) { // For each basic block BB
-				for (auto &I: BB) { // For each instruction I
-					if (I.getOpcode() == Instruction::Alloca) {
-						AllocaInst *allocInstr = dyn_cast<AllocaInst>(&I);
-						allocaMap[allocInstr->getName()] = "";
-						//cout << "New pointer name:" << allocInstr->getName().str() << "\n";
-					}
+			if (F.getName() != "main") {
+				StringMap<StringRef> allocaMap;
+				StringRef latestLoadValue;
+				//cout << "Function name:" << (&F)->getName().str() << endl;
+				for (auto &BB: F) { // For each basic block BB
+					for (auto &I: BB) { // For each instruction I
+						if (I.getOpcode() == Instruction::Alloca) {
+							AllocaInst *allocInstr = dyn_cast<AllocaInst>(&I);
+							allocaMap[allocInstr->getName()] = "";
+							//cout << "New pointer name:" << allocInstr->getName().str() << "\n";
+						}
 
-					if (I.getOpcode() == Instruction::Load) {
-						LoadInst *loadInstr = dyn_cast<LoadInst>(&I);
-						latestLoadValue = loadInstr->getPointerOperand()->getName();
-						//cout << "Load name:" << loadInstr->getPointerOperand()->getName().str() << "\n";
-					}
+						if (I.getOpcode() == Instruction::Load) {
+							LoadInst *loadInstr = dyn_cast<LoadInst>(&I);
+							latestLoadValue = loadInstr->getPointerOperand()->getName();
+							//cout << "Load name:" << loadInstr->getPointerOperand()->getName().str() << "\n";
+						}
+						
+						if (I.getOpcode() == Instruction::Store) {
+							StringRef srcValue = I.getOperand(0)->getName();
+							StringRef dstValue = I.getOperand(1)->getName();
+														
+							if ((allocaMap.find(srcValue) != allocaMap.end()) && (globalPtrVarMap.find(dstValue) != globalPtrVarMap.end())) {
+								errs() << "WARNING: pointer <" << srcValue << "> in the function <" << (&F)->getName() << "> tries to escape through the global pointer <" << dstValue << ">!" << "\n";
+							} 
+						}
 
-					if (I.getOpcode() == Instruction::Ret) {
-						ReturnInst *returnInstr = dyn_cast<ReturnInst>(&I);
-						if (returnInstr->getReturnValue() != nullptr) {
-							if (allocaMap.find(latestLoadValue) != allocaMap.end()) {
-								errs() << "WARNING: pointer <" << latestLoadValue.str() << "> in the function <" << (&F)->getName().str() << "> will not exist after the return" << "\n";
+						if (I.getOpcode() == Instruction::Ret) {
+							ReturnInst *R = dyn_cast<ReturnInst>(&I);
+							if (!R) continue;
+							Value *v = R->getReturnValue();
+							if (!v) continue;
+							GlobalAlias *ga = cast<GlobalAlias>(v);
+							if (!ga) continue;
+							Constant *c = ga->getAliasee();
+							if (!c) continue;
+							c = cast<Constant>(c->stripPointerCasts());
+							errs() << c->getName() << "\n";
+							//ReturnInst *returnInstr = dyn_cast<ReturnInst>(&I);
+							if (R->getReturnValue() != nullptr) {
+								if (allocaMap.find(c->getName()) != allocaMap.end()) {
+									errs() << "WARNING: pointer <" << c->getName() << "> in the function <" << (&F)->getName().str() << "> will not exist after the return" << "\n";
+								}
 							}
 						}
 					}
