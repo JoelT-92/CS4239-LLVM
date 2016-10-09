5f39170ff35a4f0d02f5b83522027ac7e46a8b02
JoelT-92 <JoelT-92@users.noreply.github.com>
2016-10-09 19:48:15 +0800

    Update asg2-task1.cpp

--- task1/asg2-task1.cpp
+++ task1/asg2-task1.cpp
@@ -1,61 +1,186 @@
-#define DEBUG_TYPE "bad-cast"
+#define DEBUG_TYPE "dead-function"
 #include "llvm/DebugInfo.h"
-#include "llvm/Pass.h"
-#include "llvm/IR/Constants.h"
 #include "llvm/IR/Function.h"
-#include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IRReader/IRReader.h"
-#include "llvm/Support/CallSite.h"
-#include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/CallSite.h"
 #include "llvm/ADT/StringMap.h"
-#include "llvm/ADT/SmallPtrSet.h"
-#include "llvm/Support/CFG.h"
 
-//todo : change stringref from name to function.
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace llvm;
+using namespace std;
+
+void ReportDeadFunctions();
+void InvestigateFunction(Function &F);
+Function* ResolveIndirectCall(CallSite &CS);
+void InvestigateStoreInst(StoreInst *S);
+
+vector<Function*> mainList;
+StringMap<StringRef> programFuncMap;
+StringMap<Function*> definedFuncMap;
+StringMap<Function*> pointerFuncMap;
 
 int main(int argc, char **argv) {
-	std::vector<Module*> ModuleSet;
-	for (int i = 1; i < argc; i++) {
-		// Step (1) Parse the given IR File
-		LLVMContext &Context = getGlobalContext();
-		SMDiagnostic Err;
-		Module *M = ParseIRFile(argv[i], Err, Context);
-		if (M == nullptr) {
-			fprintf(stderr, "failed to read IR file %s\n", argv[1]);
-			return 1;
-		}
-		ModuleSet.push_back(M);
-	}
 
-	// Step (2) Traverse functions for call tree
-	for (auto &M : ModuleSet) {
-		for (auto &F : *M) {
-			for (auto &BB : F) {
-				errs() << F.getName() << "\n";
-				for (auto &I : BB) {
-					AllocaInst *A = dyn_cast<AllocaInst>(&I);
-					if (A) {
-						for (Value::use_iterator i = A->use_begin(), e = A->use_end(); i != e; ++i) {
-							if (isa<StoreInst>(*i)){}//keep track of what is stored
-							if (isa<ReturnInst>(*i)) {}//keep track
-							errs() << **i << "\n";
-						}
-					} else if (isa<ReturnInst>(&I)) {
-						ReturnInst *R = dyn_cast<ReturnInst>(&I);
-						Value *V = R->getReturnValue();
-						if (V) {
-							errs() << "ret val " << *V << "\n";
-						}
-					}
+    // Step (1) Parse all IR files. 
+    // Locate all main functions and store them in mainList
+    // Assume that each main include all other files
+    for (int i = 1; i < argc; i++) {
+        LLVMContext &Context = getGlobalContext();
+        SMDiagnostic Err;
+        Module *M = ParseIRFile(argv[i], Err, Context);
+        if (M == nullptr) {
+            fprintf(stderr, "failed to read IR file %s\n", argv[1]);
+            return 1;
+        }
+        
+        // Find main, defined functions and populate maps
+        // mainList holds a list of Function pointers to main() functions
+        // programFuncMap maps key (functionName) to DEAD or ALIVE status
+        // definedFuncMap maps key (functionName) to Function pointers
+        // pointerFuncMap maps key (variableName) to Function pointers
+        Module::FunctionListType &functions = M->getFunctionList();
+        for (Module::FunctionListType::iterator it = functions.begin(), it_end = functions.end(); it != it_end; ++it) {
+            Function &func = *it;
+            
+            if (func.getName() == "main") {
+                mainList.push_back(M->getFunction(func.getName()));
+            }
+             else if ((func.getName() != "main") && (func.isDeclaration() != 1) && (programFuncMap.find(func.getName()) == programFuncMap.end())) {
+                programFuncMap[func.getName()] = "DEAD";
+                definedFuncMap[func.getName()] = M->getFunction(func.getName());
+            }
+        }
+    }
+    
+    // Step (2) Look through all main function and recurse investigate each of them
+    for(auto &F : mainList) {
+        for (auto &BB: *F) {
+            for (auto &I : BB) {
+                StoreInst *S = dyn_cast<StoreInst>(&I);
+                if (S) InvestigateStoreInst(S);
+                CallSite Call(&I);
+                if (!Call) continue;
+                Function *G = Call.getCalledFunction();
+                if (G == nullptr) {
+                    G = ResolveIndirectCall(Call);
+                    if (G == nullptr) continue;
+                }
+                programFuncMap[G->getName()] = "LIVE";
+                if (definedFuncMap[G->getName()] != 0) {
+                    InvestigateFunction(*(definedFuncMap[G->getName()]));
+                }    
+            }
+        }
+    }
+    
+    // Step (4) Report on uncalled functions
+    ReportDeadFunctions();
+}
+
+void InvestigateFunction(Function &F) {
+	for (auto &bb : F) {
+		for (auto &i : bb) {
+			StoreInst *S = dyn_cast<StoreInst>(&i);
+			if (S) InvestigateStoreInst(S);
+			CallSite Call(&i);
+			if (!Call) continue;
+			Function *G = Call.getCalledFunction();
+			if (G == nullptr) {
+				G = ResolveIndirectCall(Call);
+				if (G == nullptr) continue;
+			}
+			if (programFuncMap[G->getName()] != "LIVE") {
+				programFuncMap[G->getName()] = "LIVE";
+				if (definedFuncMap[G->getName()] != 0) { 
+					InvestigateFunction(*(definedFuncMap[G->getName()]));
 				}
+			}
+			else {
+				// Encountered a function that was already called.
+				// Prevent cycle by continue;
+				continue;
 			}
 		}
 	}
-	return 0;
+}
+
+Function* ResolveIndirectCall(CallSite &CS) {
+    Value *v = CS.getCalledValue();
+    GlobalAlias *ga = cast<GlobalAlias>(v);
+    if (!ga) return nullptr;
+    Constant *c = ga->getAliasee();
+    if (!c) return nullptr;
+    c = cast<Constant>(c->stripPointerCasts());
+	while (!isa<AllocaInst>(c) && !isa<GlobalValue>(c)) {
+		ga = cast<GlobalAlias>(c);
+		if (!ga) return nullptr;
+		c = ga->getAliasee();
+		if (!c) return nullptr;
+		c = cast<Constant>(c->stripPointerCasts());
+	}
+    StringMap<Function*>::iterator sm = pointerFuncMap.find(c->getName());
+    if (sm != pointerFuncMap.end()) {
+        return sm->second;
+    } else {
+        return nullptr;
+    }
+}
+
+void InvestigateStoreInst(StoreInst *S) {
+    Value *v = S->getValueOperand();
+    if (!v) return;
+    v = v->stripPointerCasts();
+    if (!v) return;
+    GlobalAlias *ga = cast<GlobalAlias>(S);
+    if (!ga) return;
+    Constant *c = ga->getAliasee();
+    if (!c) return;
+    c = cast<Constant>(c->stripPointerCasts());
+    if (isa<Function>(v)) {
+        pointerFuncMap[c->getName()] = dyn_cast<Function>(v);
+    } else {
+        Value *v2 = S->getPointerOperand();
+        Type* ptrType = v2->getType()->getPointerElementType();
+        if (PointerType* pt = dyn_cast<PointerType>(ptrType))
+        {
+            do {
+                Type* pointedType = pt->getTypeAtIndex((unsigned int)0);
+                if (pointedType->isFunctionTy())
+                {
+                    GlobalAlias *gga = cast<GlobalAlias>(v);
+                    if (gga) {
+						StringRef name;
+						if (isa<Argument>(gga))	name = gga->getName();
+						else name = gga->getAliasee()->getName();
+						if (pointerFuncMap.find(name) != pointerFuncMap.end()) {
+							pointerFuncMap[c->getName()] = pointerFuncMap[name];
+						} else {
+							pointerFuncMap[c->getName()] = nullptr;
+						}
+                    }
+                    break;
+                }
+                ptrType = pointedType;
+            } while ((pt = dyn_cast<PointerType>(ptrType)));
+        }
+    }
+}
+
+void ReportDeadFunctions() {
+    for (StringMap<StringRef>::const_iterator it = programFuncMap.begin(), it_end = programFuncMap.end(); it != it_end; ++it) {
+      if (it->getValue() == "DEAD") {
+          errs() << "WARNING: function <" << it->getKey() << "> is a DEAD function" << "\n";
+      }
+      else if (it->getValue() == "MAYBE DEAD") {
+          errs() << "WARNING: function <" << it->getKey() << "> might be a DEAD function" << "\n";
+      }
+    }
 }
