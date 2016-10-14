9e80a9aa9fd14cec185dcf78c2bc6b1f5072e038
JoelT-92 <JoelT-92@users.noreply.github.com>
2016-10-14 11:11:00 +0800

    Update asg2-task2.cpp

--- task2/asg2-task2.cpp
+++ task2/asg2-task2.cpp
@@ -1,6 +1,7 @@
 #define DEBUG_TYPE "escape"
 #include "llvm/DebugInfo.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Constants.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
@@ -15,8 +16,6 @@
 
 using namespace llvm;
 using namespace std;
-
-StringMap<StringRef> globalPtrVarMap;
 
 int main(int argc, char **argv) {
     
@@ -35,38 +34,83 @@ int main(int argc, char **argv) {
         // Step (2) Traverse all instructions
         for (auto &F: *M) { // For each function F
             if (F.getName() != "main") {
-                StringMap<StringRef> allocaMap;
+                multimap<Value*,Value*> gayMap;
+                map<Value*,size_t> lineMap;
+                size_t line = 1;
                 for (auto &BB: F) { // For each basic block BB
                     for (auto &I: BB) { // For each instruction I
                         if (I.getOpcode() == Instruction::Alloca) {
-                            AllocaInst *allocInstr = dyn_cast<AllocaInst>(&I);
-                            allocaMap[allocInstr->getName()] = "";
-                        }
-                        
-                        if (I.getOpcode() == Instruction::Ret) {
-                            ReturnInst *R = dyn_cast<ReturnInst>(&I);
-                            if (!R) continue;
-                            Value *v = R->getReturnValue();
-                            if (!v) continue;
-                            GlobalAlias *ga = cast<GlobalAlias>(v);
-                            if (!ga) continue;
-                            Constant *c = ga->getAliasee();
-                            if (!c) continue;
-                            c = cast<Constant>(c->stripPointerCasts());
-                            while (!isa<AllocaInst>(c) && !isa<GlobalValue>(c)) {
-                                ga = cast<GlobalAlias>(c);
-                                if (!ga) goto end;
-                                c = ga->getAliasee();
-                                if (!c) goto end;
-                                c = cast<Constant>(c->stripPointerCasts());
-                            }
-                            if (R->getReturnValue() != nullptr) {
-                                if (allocaMap.find(c->getName()) != allocaMap.end()) {
-                                    errs() << "WARNING: variable <" << c->getName() << "> in the function <" << (&F)->getName().str() << "> will not exist after the return" << "\n";
+                            Value *v = dyn_cast<Value>(&I);
+                            gayMap.insert(pair<Value*,Value*>(v, 0));
+                        } else if (I.getOpcode() == Instruction::Load) {
+                            LoadInst *R = dyn_cast<LoadInst>(&I);
+                            Value *v = dyn_cast<Value>(&I);
+                            Value *vv = R->getPointerOperand();
+                            gayMap.insert(pair<Value*,Value*>(v, vv));
+                        } else if (I.getOpcode() == Instruction::GetElementPtr) {
+                            GetElementPtrInst *R = dyn_cast<GetElementPtrInst>(&I);
+                            Value *v = dyn_cast<Value>(&I);
+                            Value *vv = R->getPointerOperand();
+                            gayMap.insert(pair<Value*,Value*>(v, vv));
+                        } else if (I.getOpcode() == Instruction::Store) {
+                            StoreInst *R = dyn_cast<StoreInst>(&I);
+                            Value *v = R->getPointerOperand();
+                            Value *vv = R->getValueOperand();
+                            if (isa<ConstantExpr>(vv)) {
+                                ConstantExpr *ce = dyn_cast<ConstantExpr>(vv);
+                                if (isa<GlobalValue>(ce->getOperand(0))) {
+                                    gayMap.insert(pair<Value*,Value*>(v, vv));
+                                    if (vv && gayMap.find(vv) == gayMap.end()) {
+                                        gayMap.insert(pair<Value*,Value*>(vv, 0));
+                                    }
+
+                                }
+                            } else if (isa<Constant>(vv)) { continue; }
+                            else {
+                                gayMap.insert(pair<Value*,Value*>(v, vv));
+                                if (vv && gayMap.find(vv) == gayMap.end()) {
+                                    gayMap.insert(pair<Value*,Value*>(vv, v));
                                 }
                             }
-                            end: continue;
+                        } else if (isa<UnaryInstruction>(&I)) {
+                            Value *v = dyn_cast<Value>(&I);
+                            Value *vv = I.getOperand(0);
+                            gayMap.insert(pair<Value*,Value*>(v, vv));
+                        } else if (I.getOpcode() == Instruction::Ret) {    //for the program, its because the alloca is surprisingly at a higher address than the rest
+                            ReturnInst *R = dyn_cast<ReturnInst>(&I);
+                            if (R) {
+                                Value *v = R->getReturnValue();
+                                if (v && gayMap.size() > 0) {
+                                    pair<multimap<Value*,Value*>::iterator,multimap<Value*,Value*>::iterator> ret;
+                                    Value *nextV = 0, *prevV = dyn_cast<Value>(R), *first;
+                                    while (true) {
+                                        ret = gayMap.equal_range(v);
+                                        for (multimap<Value*,Value*>::iterator it = ret.first; it != ret.second; ++it) {
+                                            if (isa<AllocaInst>(v)) {
+                                                if (lineMap[it->second] < lineMap[prevV]) {
+                                                    nextV = it->second;
+                                                }
+                                            } else {
+                                                if (lineMap[it->second] <= lineMap[v]) {
+                                                    nextV = it->second;
+                                                    prevV = v;
+                                                }
+                                            }
+                                        }
+                                        if (nextV == 0 || v == nextV) {                                            
+                                            if (v && v->getName() != "") {
+                                                errs() << v->getName() << " escaped \n";
+                                            }
+                                            break;
+                                        }
+                                        v = nextV;
+                                    }
+                                }
+                            }
                         }
+                        Value *v = dyn_cast<Value>(&I);
+                        lineMap[v] = line;
+                        line++;
                     }
                 }
             }
